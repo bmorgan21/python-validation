bf133dc92ada0274f6ee3c62ab828e193a4707b6
Brian Morgan <brian.s.morgan@gmail.com>
2016-11-15 17:39:40 -0500

    bump version again, pypi issue

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='validation21',
     packages=['validation21'],
-    version='0.2.3',
+    version='0.2.4',
     description='Validation library for Python.',
     author='Brian S Morgan',
     author_email='brian.s.morgan@gmail.com',
